a1d4aad44ec3d89b4ae0ca21737d83a710d28831
YotaNakamura <b1016143@fun.ac.jp>
2017-10-10 17:09:30 +0900

    ラベルとボタンを接続し、値を変数で仮置き

--- BusRomance/Swift/ViewController.swift
+++ BusRomance/Swift/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var reloadButton: UIButton!
+    @IBOutlet weak var busStopLabel1: UILabel!
+    @IBOutlet weak var busStopLabel2: UILabel!
+    @IBOutlet weak var fareLabel: UILabel!
+    @IBOutlet weak var busTimeLabel1: UILabel!
+    @IBOutlet weak var busTimeLabel2: UILabel!
+    @IBOutlet weak var busRemainLabel1: UILabel!
+    @IBOutlet weak var busRemainLabel2: UILabel!
+    
+    var fare:Int = 190 //乗車運賃
+    var busRem1:Int = 3 //乗車までの時間1
+    var busRem2:Int = 65 //乗車までの時間2
+    var busStop1:String = "はこだて未来大学" //乗車バス停名
+    var busStop2:String = "赤川通" //降車バス停名
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        busStopLabel1.text = "\(busStop1)"
+        busStopLabel2.text = "\(busStop2)"
+        fareLabel.text = "\(fare) 円"
+        busRemainLabel1.text = "到着まで約 \(busRem1) 分"
+        busRemainLabel2.text = "到着まで約 \(busRem2) 分"
     }
 
     override func didReceiveMemoryWarning() {
