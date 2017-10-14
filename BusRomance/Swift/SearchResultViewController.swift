2b824e80deb08798ba2effa2ccd55169376a4571
ARATAYOKOYAMA <bob2525@hotmail.co.jp>
2017-10-15 01:36:42 +0900

    横山 [add]検索結果画面への値渡し&サーバーへのPOST,GETの仮実装

---
+++ BusRomance/Swift/SearchResultViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SearchResultViewController.swift
+//  BusRomance
+//
+//  Created by 横山　新 on 2017/10/14.
+//  Copyright © 2017年 バスロマン. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController{
+    
+    @IBOutlet weak var departureTextField: UILabel!
+    @IBOutlet weak var arrivalTextField: UILabel!
+    
+    var departureBusStop = ""
+    var arrivalBusStop = ""
+    
+    override func viewDidLoad() {
+        departureTextField.text = departureBusStop
+        arrivalTextField.text = arrivalBusStop
+    }
+    
+    override func didReceiveMemoryWarning() {
+        
+    }
+}
