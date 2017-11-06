18830ad7428869826bd03964429f485d9d9ee69d
YotaNakamura <atoy.ryujin.93621339@gmail.com>
2017-11-06 15:22:09 +0900

    画像素材追加しました（ふきだし、街並み背景）

--- BusRomance/Swift/ViewController.swift
+++ BusRomance/Swift/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
     
@@ -18,21 +19,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var busTimeLabel2: UILabel!
     @IBOutlet weak var busRemainLabel1: UILabel!
     @IBOutlet weak var busRemainLabel2: UILabel!
+
     
     var fare:Int = 190 //乗車運賃
     var busRem1:Int = 3 //乗車までの時間1
     var busRem2:Int = 65 //乗車までの時間2
-    var busStop1:String = "はこだて未来大学" //乗車バス停名
-    var busStop2:String = "赤川通" //降車バス停名
+    var busStop1:String = "乗車バス停名" //乗車バス停名
+    var busStop2:String = "はこだて未来大学" //降車バス停名
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         //グラデーションの開始色(朝)
-        let topColor = UIColor(red:0.416, green:0.608, blue:0.784, alpha:1)
+        let topColor = UIColor(red:0.000, green:0.557, blue:1.000, alpha:1)
+        
         //グラデーションの終了色(朝)
-        let bottomColor = UIColor(red:0.459, green:0.996, blue:0.992, alpha:1)
+        let bottomColor = UIColor(red:0.000, green:1.000, blue:1.000, alpha:0.0)
+        
+        //let topColor = UIColor(red:0.980, green:0.439, blue:0.604, alpha:1)//夕方
+        
+        //let bottomColor = UIColor(red:0.996, green:0.882, blue:0.251, alpha:1)//夕方
+        
+        // let topColor = UIColor(red:0.200, green:0.031, blue:0.404, alpha:1)//夜
+        
+        // let bottomColor = UIColor(red:0.108, green:0.442, blue:0.746, alpha:1)//夜
         
         //グラデーションの色を配列で管理
         let gradientColors: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
@@ -53,6 +64,10 @@ class ViewController: UIViewController {
         fareLabel.text = "\(fare) 円"
         busRemainLabel1.text = "到着まで約 \(busRem1) 分"
         busRemainLabel2.text = "到着まで約 \(busRem2) 分"
+        let realm = try! Realm()
+        let getOnBusStop = realm.objects(FrequentlyPlaceObject.self)
+        busStopLabel1.text = "\(getOnBusStop.last!.busStop)"
+        
     }
 
     override func didReceiveMemoryWarning() {
