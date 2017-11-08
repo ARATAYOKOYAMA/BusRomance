ac2d622588fb57f65c14fd897ece7de9e0d7e7c1
YotaNakamura <atoy.ryujin.93621339@gmail.com>
2017-11-08 18:25:37 +0900

    ホーム画面レイアウト修正、更新ボタン素材置換

--- BusRomance/Swift/ViewController.swift
+++ BusRomance/Swift/ViewController.swift
@@ -19,14 +19,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var busTimeLabel2: UILabel!
     @IBOutlet weak var busRemainLabel1: UILabel!
     @IBOutlet weak var busRemainLabel2: UILabel!
-
+    @IBOutlet weak var hayaLabel: UILabel!
+    @IBOutlet weak var tsugiLabel: UILabel!
     
     var fare:Int = 190 //乗車運賃
     var busRem1:Int = 3 //乗車までの時間1
     var busRem2:Int = 65 //乗車までの時間2
 
     var busStop1:String = "はこだて未来大学" //乗車バス停名
-    var busStop2:String = "赤川通" //降車バス停名
+    var busStop2:String = "はこだて未来大学" //降車バス停名
     var topColor:UIColor = UIColor(red:0.000, green:0.000, blue:0.000, alpha:1)
     var bottomColor:UIColor = UIColor(red:0.000, green:0.000, blue:0.000, alpha:1)
     
@@ -35,7 +36,7 @@ class ViewController: UIViewController {
         //deleateRealm2()
         busStopLabel1.text = "\(busStop1)"
         busStopLabel2.text = "\(busStop2)"
-        fareLabel.text = "\(fare) 円"
+        fareLabel.text = "\(fare)円"
         busRemainLabel1.text = "到着まで約 \(busRem1) 分"
         busRemainLabel2.text = "到着まで約 \(busRem2) 分"
         let realm = try! Realm()
@@ -46,17 +47,31 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
         var nowTimeString = getNowClockString()
-        var nowTimeInt:Int = Int(nowTimeString)!
+        var nowTimeInt:Int = Int(nowTimeString)!+2
         if nowTimeInt >= 6 && nowTimeInt <= 15{
-            topColor = UIColor(red:0.000, green:0.357, blue:0.918, alpha:1)//朝
-            bottomColor = UIColor(red:0.455, green:0.822, blue:0.835, alpha:0.4)//朝
+            topColor = UIColor(red:0.000, green:0.557, blue:1.000, alpha:1)//朝
+            bottomColor = UIColor(red:0.000, green:1.000, blue:1.000, alpha:0)//朝
+            hayaLabel.backgroundColor = UIColor(red:0.000, green:0.557, blue:1.000, alpha:1)
+            busStopLabel1.backgroundColor = UIColor(red:0.000, green:0.707, blue:1.000, alpha:0.7)
+            busStopLabel2.backgroundColor = UIColor(red:0.000, green:0.707, blue:1.000, alpha:0.7)
+            hayaLabel.backgroundColor =  UIColor(red:0.000, green:0.777, blue:1.000, alpha:0.7)
+            tsugiLabel.backgroundColor =  UIColor(red:0.000, green:0.777, blue:1.000, alpha:0.7)
         }else if nowTimeInt >= 16 && nowTimeInt <= 18{
             topColor = UIColor(red:0.980, green:0.439, blue:0.604, alpha:1)//夕方
             bottomColor = UIColor(red:0.996, green:0.882, blue:0.251, alpha:1)//夕方
+            busStopLabel1.backgroundColor = UIColor(red:0.985, green:0.587, blue:0.484, alpha:1)
+            busStopLabel2.backgroundColor = UIColor(red:0.985, green:0.587, blue:0.484, alpha:1)
+            hayaLabel.backgroundColor =  UIColor(red:0.990, green:0.659, blue:0.421, alpha:1)
+            tsugiLabel.backgroundColor =  UIColor(red:0.990, green:0.659, blue:0.421, alpha:1)
         }else if nowTimeInt >= 19 && nowTimeInt <= 24 || nowTimeInt >= 0 && nowTimeInt <= 5{
             topColor = UIColor(red:0.200, green:0.031, blue:0.404, alpha:1)//夜
             bottomColor = UIColor(red:0.108, green:0.442, blue:0.746, alpha:1)//夜
+            busStopLabel1.backgroundColor = UIColor(red:0.170, green:0.170, blue:0.520, alpha:1)
+            busStopLabel2.backgroundColor = UIColor(red:0.170, green:0.170, blue:0.520, alpha:1)
+            hayaLabel.backgroundColor =  UIColor(red:0.150, green:0.240, blue:0.570, alpha:1)
+            tsugiLabel.backgroundColor =  UIColor(red:0.150, green:0.240, blue:0.570, alpha:1)
         }
         print(nowTimeString)
         let gradientColors: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
