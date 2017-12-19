a318524a88e534a01c92036a5b886da941388edb
Kodai Sato <g2117023@fun.ac.jp>
2017-12-19 21:14:00 +0900

    今日のバス情報を表示するviewを作成・autolayout設定

--- BusRomance/Swift/HomeSecondViewController.swift
+++ BusRomance/Swift/HomeSecondViewController.swift
@@ -14,12 +14,42 @@ class HomeSecondViewController: UIViewController {
     var tapEndPosX:CGFloat = 0
     var tapEndPosY:CGFloat = 0
     
+    
+    @IBOutlet weak var todayLabel: UILabel! //「今日」と表示するラベル
+    @IBOutlet weak var todayDateLabel: UILabel! //今日の日付を表示するラベル
+    @IBOutlet weak var todayView: UIView! //今日の情報を表示する白いview
+    @IBOutlet weak var todayFirstScheduleLabel: UILabel! //「行」の講義名を表示するラベル
+    @IBOutlet weak var todayFirstTimeLabel: UILabel! //「行」のバスの時刻を表示するラベル
+    @IBOutlet weak var todayFirstBusLabel: UILabel! //「行」のバス系統名を表示するラベル
+    @IBOutlet weak var todayFirstBusStopLabel: UILabel! //「行」の乗車停留所名を表示するラベル
+    @IBOutlet weak var todayLastScheduleLabel: UILabel! //「帰」の講義名を表示するラベル
+    @IBOutlet weak var todayLastTimeLabel: UILabel! //「帰」のバスの時刻を表示するラベル
+    @IBOutlet weak var todayLastBusLabel: UILabel! //「帰」のバス系統名を表示するラベル
+    @IBOutlet weak var todayLastBusStopLabel: UILabel! //「帰」の乗車停留所名を表示するラベル
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGesture(sender:)))
         secondView.addGestureRecognizer(panGestureRecognizer)
+        
+        /* --- Label・Viewの初期設定 --- */
+        todayView.layer.cornerRadius = 20 //白いviewに角丸を設定
+        todayView.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.8) //背景色を半透明化
+        
+        /* --- ラベルの初期値を設定.（現在は仮） ---*/
+        //「今日（上のview）」のラベル
+        todayLabel.text = "今日"
+        todayDateLabel.text = "2017/02/02"
+        todayFirstScheduleLabel.text = "1限　AAAAAAAAAAAAAAAA"
+        todayFirstTimeLabel.text = "12:00"
+        todayFirstBusLabel.text = "105系統"
+        todayFirstBusStopLabel.text = "赤川入口"
+        todayLastScheduleLabel.text = "1限　AAAAAAA"
+        todayLastTimeLabel.text = "12:00"
+        todayLastBusLabel.text = "105系統"
+        todayLastBusStopLabel.text = "赤川入口"
     }
 
     override func didReceiveMemoryWarning() {
